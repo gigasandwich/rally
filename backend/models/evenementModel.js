e36dbfae6611454db0c9aeadaefea91fb0884c34
Yvan Noah <yvannandy@gmail.com>
2025-03-28 18:55:46 +0300

    Remove backend/index.js
    Server.js dia efa ampy

--- backend/models/evenementModel.js
+++ backend/models/evenementModel.js
@@ -4,7 +4,7 @@ const path = require('path');
 let DBQ = path.join(__dirname, '../../database/rally-2007.accdb');
 const connectionString = `Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=${DBQ};`;
 
-const getEvenements = (callback) => {
+const getEvenements = (callback) => { // Callback: 1er arg: erreur/exception, 2e arg: ilay resultat attendu. Iray amreo null fona rehefa retourne
   const query = 'SELECT * FROM Evenement';
   sql.query(connectionString, query, (err, rows) => {
     if (err) {
--- backend/models/voitureModel.js
+++ backend/models/voitureModel.js
@@ -5,7 +5,6 @@ let DBQ = path.join(__dirname, '../../database/rally-2007.accdb');
 const connectionString = `Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=${DBQ};`;
 
 const getVoitures = (callback) => {
-    console.log(`Connection string: ${connectionString}`);
     const query = 'SELECT * FROM voiture';
     sql.query(connectionString, query, (err, rows) => {
         if (err) {
