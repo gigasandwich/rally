21a66a44e3bc9282e18807fb708530e366b9a745
Yvan Noah <yvannandy@gmail.com>
2025-03-28 11:07:58 +0300

    Fix data fetching

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors({ origin: 'http://localhost:4321' })); // Manala resaka probleme de header
 app.use(express.json()); // Parsen le app automatique ho json ny json request tonga avy any (tsy mila miparse manuel)
 
 const voitures = [
