cf5b2144177d2168d9ba1c3e596aad4eb29b61af
HarySteeve <grandturismo2005@gmail.com>
2025-03-28 17:12:58 +0100

    moduf the dsn
    I create a new file dsn.js which contains the method getDsn(). It's get the dsn of the Database

--- backend/models/voitureModel.js
+++ backend/models/voitureModel.js
@@ -1,8 +1,9 @@
 const sql = require('msnodesqlv8');
-const path = require('path');
+const dsn = require('./dsn');
 
-let DBQ = path.join(__dirname, '../../database/rally-2007.accdb');
-const connectionString = `Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=${DBQ};`;
+// let DBQ = path.join(__dirname, '../../database/rally-2007.accdb');
+// const connectionString = `Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=${DBQ};`;
+const connectionString = dsn.getDsn();
 
 const getVoitures = (callback) => {
     console.log(`Connection string: ${connectionString}`);
